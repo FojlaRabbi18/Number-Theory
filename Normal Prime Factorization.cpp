cd47109f26cbe125ef611cc42e0d72eab817c361
Fojla Rabbi <fojlarabbi018@gmail.com>
2025-11-19 08:51:20 +0600

    Update Normal Prime Factorization.cpp

--- Normal Prime Factorization.cpp
+++ Normal Prime Factorization.cpp
@@ -1,5 +1,7 @@
 // O(sqrt(n)) ---> For prime
-// O(log n) ---> For composite
+// O(log n) ---> For composite 
+// (a number n can have max log n prime divisors, say all are 2s in the P. Factorization)
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -9,7 +11,7 @@ void solve() {
     vector<int> v;
     for(int i = 2; i*i <= n; i++) { 
         if(n % i == 0) {
-            while(n % i == 0) { // Always think n in skeleton form
+            while(n % i == 0) { // Always think n in skeleton form(PF)
                 v.push_back(i);
                 n /= i;
             }
